f7b1587c4a490cf86db43b0e8c78f7320466f747
Jyoti <sainijyoti042002@gmail.com>
2024-08-11 11:58:29 +0530

    new

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
+
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('./config/db'); //  this is pointing to the updated db config
+// const Product = require('./models/Product');
+// require('dotenv').config();
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// // Use cors middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Add product endpoint
+// app.post('/add-product', async (req, res) => {
+//   const { name, category, brand, price, description, photos } = req.body;
+//   try {
+//     const newProduct = new Product({
+//       name,
+//       category,
+//       brand,
+//       price,
+//       description,
+//       photos
+//     });
+//     await newProduct.save();
+//     res.status(201).json(newProduct);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Something went wrong' });
+//   }
+// });
+
+// // Fetch all products endpoint
+// app.get('/products', async (req, res) => {
+//   try {
+//     const products = await Product.find();
+//     res.json(products);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Something went wrong' });
+//   }
+// });
+
+// // Update product endpoint
+// app.put('/products/update/:id', async (req, res) => {
+//   const { id } = req.params;
+//   const { name, category, brand, price, description, photos } = req.body;
+
+//   try {
+//     const product = await Product.findById(id);
+//     if (product) {
+//       product.name = name;
+//       product.category = category;
+//       product.brand = brand;
+//       product.price = price;
+//       product.description = description;
+//       product.photos = photos;
+
+//       await product.save();
+//       res.status(200).json({ message: 'Product updated successfully', product });
+//     } else {
+//       res.status(404).json({ error: 'Product not found' });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error: 'Something went wrong' });
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('./config/db'); // Import the database connection
@@ -43,13 +114,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // CORS configuration
-const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || 'http://localhost:3001', // Allow this origin, use env variable if needed
-  methods: 'GET,POST,PUT,DELETE', // Allowed methods
-  allowedHeaders: 'Content-Type,Authorization', // Allowed headers
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Add product endpoint
